3ed7e1f7ab009df11d5e1a4dda16ed042f9747d4
Muhammad Tifak <moht20009@gmail.com>
2020-02-18 16:47:11 -0800

    Getting value input into search form

--- main.js
+++ main.js
@@ -5,3 +5,9 @@ searchForm.addEventListener('submit', submitSearch)
 function submitSearch(e) {
   // The form was submitted!
 }
+
+function submitSearch(e) {
+  // Get the value input in the search input
+  const query = inputSearch.value
+
+}
